6900c1cfb269d69bba5fd34ec36ae47cf2c22c27
borissvoboda <svoboda.boris@gmail.com>
2025-12-03 23:11:49 +0100

    02

---
+++ 02-section/01/app.go
@@ -0,0 +1,25 @@
+// Lesson 14nn
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go!")
+}
+
+/*
+Lesson 14
+Package is main, but not module.
+One module may consist from multiple packages.
+
+go mod init <name, or path where it can be found>
+
+Go needs main module.
+
+
+Lesson 15
+Only 1 main function.
+
+
+*/
