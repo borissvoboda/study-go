ff21a019810b7d11ccd808dcba33adb4934e7bca
borissvoboda <svoboda.boris@gmail.com>
2025-12-04 21:36:11 +0100

    investment and cleanup

---
+++ max/02-section/16nn/investment_calculator.go
@@ -0,0 +1,14 @@
+// go mod init example.com/investment-calculator
+
+package main
+
+//import "fmt"
+
+func main() {
+	var investmentAmount = 1000
+	var expoctedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + expectedRate / 100)
+}
+
